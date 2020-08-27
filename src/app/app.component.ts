4cadd15a2ec01151ae8f1d9726574dccff0d39ec
Hieu Pham <hieu.pham01@student.csulb.edu>
2020-08-27 17:19:15 +0700

    Added previous src and example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
-import { trigger, state, style, animate, transition } from '@angular/animations';
 import { faCalendarAlt, faFlag, faThumbsUp, faSearch, faArrowCircleLeft, faArrowCircleRight, faPhone, faEnvelope, faMapMarkerAlt, faFax, faIdBadge, faKey, faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  animations: [
-    trigger('smoothCollapse', [
-      state('initial', style({
-        height:'0',
-        overflow:'hidden',
-        opacity:'0'
-      })),
-      state('final', style({
-        overflow:'hidden',
-        opacity:'1'
-      })),
-      transition('initial=>final', animate('750ms')),
-      transition('final=>initial', animate('750ms'))
-    ]),
-  ]
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
